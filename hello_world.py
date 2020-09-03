d005fe7ae33e342d8bb493cff22058d20afbcc6a
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2020-09-03 21:13:06 +0530

    basic python executable file

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# the above line tells the OS to look for the program to execute this file
+# We need to make this file executable by following command in CLI
+# chmod +x hello_world.py
+print("Hello World")
